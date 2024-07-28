b5f911d84ade0ae3854ae48e5ae14bff62e3a698
Revvxs <151439160+Revvxs@users.noreply.github.com>
2024-07-27 18:26:57 -0700

    Create update_tool.sh

---
+++ update_tool.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Navigate to the repository directory
+cd $HOME/TermuxToolHub
+
+# Pull the latest changes from GitHub
+git pull origin main
+
+echo "Tool updated to the latest version."
